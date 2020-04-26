d7e0c11edec80a80c056786db2c3205f2d7c5a15
Eva Comaroski <eva.comaroski@gmail.com>
2020-04-26 04:01:00 +0300

    Tested with WeMoS D1 Pro (ESP8266) and ESP32 Dev Module.
    * move configurable parameters to the top of the file and add sections.
    * turn some configurable parameters into defines rather than variables.
    * change from DyanmicJsonDocument to StaticJsonDocument in order to suit arduinojson v6
    * allow the user to switch between SSL and plain HTTP (for private servers)
    * fix deep sleep support for ESP32 and ESP8266 (ESP8266 is now compatible)
    * remove yield() in the main loop since it is redundant
    * rename GROUP_NAME -> FAMILY_NAME since that is what is implied (find3 terminology)
    * fix defines to include BLE only on ESP32
    * fix a startup crash on ESP8266 (wait for WiFI connection)
    * use proper NTP client to retrieve timestamp and remove getUnixTimestamp (now compatible with both ESP32 and ESP8266)
    * add the UTC offset as a parameter to the NTP client
    * shorten the chip ID and add "esp-" to make it clear what device is updating the find server
    * use toggle defines rather than "is defined" since it is more elegant for the user to deal with

--- esp-find3-client.ino
+++ esp-find3-client.ino
@@ -2,21 +2,75 @@
   This file is part of esp-find3-client by Sylwester aka DatanoiseTV.
   The original source can be found at https://github.com/DatanoiseTV/esp-find3-client.
 
+  26/04/2020: Adjustements by Wizardry and Steamworks.
+
   esp-find3-client is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation, either version 3 of the License, or
   (at your option) any later version.
-  
+
   esp-find3-client is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
-  
+
   You should have received a copy of the GNU General Public License
   along with esp-find3-client.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#ifdef ESP32
+///////////////////////////////////////////////////////////////////////////
+//                             CONFIGURATION                             //
+///////////////////////////////////////////////////////////////////////////
+
+// Set to the WiFi AP name.
+#define WIFI_SSID ""
+// Set to the WiFi AP password.
+#define WIFI_PSK ""
+
+// Set to 1 for learning mode.
+#define MODE_LEARNING 0
+#define LOCATION ""
+
+// Family name.
+#define FAMILY_NAME ""
+
+// BLE requires large app partition or the sketch will not fit.
+// Please pick either of:
+// * Tools -> Partition scheme -> Minimal SPIFFS (1.9MB APP / 190KB SPIFFS)
+// * Tools -> Partition scheme -> Huge App (3MB No OTA / 1MB SPIFFS)
+// Set to 1 to enable BLE.
+#define USE_BLE 0
+#define BLE_SCANTIME 5
+
+// Set to 1 for power saving. This will make it run less often.
+#define USE_DEEPSLEEP 0
+
+// 20 currently results in an interval of 45s
+#define TIME_TO_SLEEP  20        /* Time ESP32 will go to sleep (in seconds) */
+
+// Official server: cloud.internalpositioning.com
+#define FIND_HOST "cloud.internalpositioning.com"
+// Official port: 443 and SSL set to 1
+#define FIND_PORT 443
+// Whether to use SSL for the HTTP connection.
+// Set to 1 for official cloud server.
+#define USE_HTTP_SSL 1
+// Timeout connecting to find3 server expressed in milliseconds.
+#define HTTP_TIMEOUT 2500
+
+// The NTP server to use for time updates.
+#define NTP_HOST "pool.ntp.org"
+// The offset in seconds from UTC, ie: 3600 for +1 Hour.
+#define UTC_OFFSET 0
+
+// Set to 1 to enable. Used for verbose debugging.
+#define DEBUG 0
+
+///////////////////////////////////////////////////////////////////////////
+//                              INTERNALS                                //
+///////////////////////////////////////////////////////////////////////////
+
+#ifdef ARDUINO_ARCH_ESP32
 #include <WiFiClientSecure.h>
 #include <WiFi.h>
 #include <WiFiMulti.h>
@@ -25,29 +79,28 @@ WiFiMulti wifiMulti;
 #include <ESP8266WiFi.h>
 #include <ESP8266WiFiMulti.h>
 #include <time.h>
-
 ESP8266WiFiMulti wifiMulti;
 #endif
+#include <WiFiUdp.h>
+#include <NTPClient.h>
+
+#if defined(ARDUINO_ARCH_ESP8266)
+#define GET_CHIP_ID() String(ESP.getChipId(), HEX)
+#elif defined(ARDUINO_ARCH_ESP32)
+#define GET_CHIP_ID() String(((uint16_t)(ESP.getEfuseMac()>>32)), HEX)
+#endif
+
+#define uS_TO_S_FACTOR 1000000  /* Conversion factor for micro seconds to seconds */
 
 #define ARDUINOJSON_USE_LONG_LONG 1
 #include <ArduinoJson.h>
 
-const char* ssid     = "WIFI_SSID";
-const char* password = "WIFI_PASS";
+// Automagically disable BLE on ESP8266
+#if defined(ARDUINO_ARCH_ESP8266)
+#define USE_BLE 0
+#endif
 
-// Uncomment to set to learn mode
-// #define MODE_LEARNING 1
-#define LOCATION "bath room"
-
-#define GROUP_NAME "somegroup"
-
-// Important! BLE + WiFi Support does not fit in standard partition table.
-// Manual experimental changes are needed.
-// See https://desire.giesecke.tk/index.php/2018/01/30/change-partition-size/
-//#define USE_BLE 1
-#define BLE_SCANTIME 5
-
-#ifdef USE_BLE
+#if (USE_BLE == 1)
 #include <BLEDevice.h>
 #include <BLEUtils.h>
 #include <BLEScan.h>
@@ -59,43 +112,26 @@ class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
 };
 #endif
 
-// Automagically disable BLE on ESP8266
-#if defined(ESP8266)
-#undef USE_BLE
-#endif
-
-// Enable Deepsleep for power saving. This will make it run less often.
-// #define USE_DEEPSLEEP 1
-
-#define uS_TO_S_FACTOR 1000000  /* Conversion factor for micro seconds to seconds */
-
-// 20 currently results in an interval of 45s
-#define TIME_TO_SLEEP  20        /* Time ESP32 will go to sleep (in seconds) */
-
-#ifdef ESP32
+#ifdef ARDUINO_ARCH_ESP32
 RTC_DATA_ATTR int bootCount = 0;
 #endif
 
-const char* host = "cloud.internalpositioning.com";
-const char* ntpServer = "pool.ntp.org";
-
-//#define DEBUG 1
-
-String chipIdStr;
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, NTP_HOST, UTC_OFFSET, 60000);
 
 /*
-Method to print the reason by which ESP32
-has been awaken from sleep
+  Method to print the reason by which ESP32
+  has been awaken from sleep
 */
-void print_wakeup_reason(){
-  #ifdef ESP32
+void print_wakeup_reason() {
+#ifdef ARDUINO_ARCH_ESP32
   esp_sleep_wakeup_cause_t wakeup_reason;
 
   wakeup_reason = esp_sleep_get_wakeup_cause();
   ++bootCount;
   Serial.println("[ INFO ]\tBoot number: " + String(bootCount));
-   
-  switch(wakeup_reason)
+
+  switch (wakeup_reason)
   {
     case 1  : Serial.println("[ INFO ]\tWakeup caused by external signal using RTC_IO"); break;
     case 2  : Serial.println("[ INFO ]\tWakeup caused by external signal using RTC_CNTL"); break;
@@ -104,72 +140,20 @@ void print_wakeup_reason(){
     case 5  : Serial.println("[ INFO ]\tWakeup caused by ULP program"); break;
     default : Serial.println("[ INFO ]\tWakeup was not caused by deep sleep"); break;
   }
-  #endif
-}
-
-void setup() {
-  Serial.begin(115200);
-  delay(1000);
-
-  #ifdef USE_BLE
-  Serial.println("Find3 ESP client by DatanoiseTV (WiFi + BLE support.)");
-  #else
-  Serial.println("Find3 ESP client by DatanoiseTV (WiFi support WITHOUT BLE.)");
-  #endif
-
-  print_wakeup_reason();
-  #ifdef ESP32
-  chipIdStr = String((uint32_t)(ESP.getEfuseMac()>>16));
-  #else
-  chipIdStr = String(ESP.getChipId());
-  #endif
-  
-  Serial.print("[ INFO ]\tChipID is: ");
-  Serial.println(chipIdStr);
-
-  wifiMulti.addAP(ssid, password);
-
-  Serial.println("[ INFO ]\tConnecting to WiFi..");
-  if (wifiMulti.run() == WL_CONNECTED) {
-    Serial.println("[ INFO ]\tWiFi connection established.");
-    Serial.print("[ INFO ]\tIP address: ");
-    Serial.println(WiFi.localIP());
-    configTime(0, 0, ntpServer);
-  }
-}
-
-unsigned long long getUnixTime() {
-  #ifdef ESP32
-  time_t now;
-  struct tm timeinfo;
-  if (!getLocalTime(&timeinfo)) {
-    Serial.println("[ ERROR ]\tFailed to obtain time via NTP. Retrying.");
-    getUnixTime();
-  }
-  else
-  {
-    Serial.println("[ INFO ]\tSuccessfully obtained time via NTP.");
-  }
-  time(&now);
-  unsigned long long uTime = (uintmax_t)now;
-  return uTime * 1000UL;
-  #else
-  return 123456;
-  #endif
+#endif
 }
 
 void SubmitWiFi(void)
 {
   String request;
-  uint64_t chipid;  
 
-  DynamicJsonBuffer jsonBuffer;
+  StaticJsonDocument<256> jsonBuffer;
+  JsonObject root = jsonBuffer.to<JsonObject>();
 
-  JsonObject& root = jsonBuffer.createObject();
-  root["d"] = chipIdStr;
-  root["f"] = GROUP_NAME;
-  root["t"] = getUnixTime();
-  JsonObject& data = root.createNestedObject("s");
+  root["d"] = "esp-" + GET_CHIP_ID();
+  root["f"] = FAMILY_NAME;
+  root["t"] = timeClient.getEpochTime();
+  JsonObject data = root.createNestedObject("s");
 
   Serial.println("[ INFO ]\tWiFi scan starting..");
   int n = WiFi.scanNetworks(false, true);
@@ -180,12 +164,12 @@ void SubmitWiFi(void)
     Serial.print("[ INFO ]\t");
     Serial.print(n);
     Serial.println(" WiFi networks found.");
-    JsonObject& wifi_network = data.createNestedObject("wifi");
+    JsonObject wifi_network = data.createNestedObject("wifi");
     for (int i = 0; i < n; ++i) {
       wifi_network[WiFi.BSSIDstr(i)] = WiFi.RSSI(i);
     }
 
-    #ifdef USE_BLE
+#if (USE_BLE == 1)
     Serial.println("[ INFO ]\tBLE scan starting..");
     BLEDevice::init("");
     BLEScan* pBLEScan = BLEDevice::getScan(); // create new scan
@@ -197,30 +181,34 @@ void SubmitWiFi(void)
     Serial.print(foundDevices.getCount());
     Serial.println(" BLE devices found.");
 
-    JsonObject& bt_network = data.createNestedObject("bluetooth");
-    for(int i=0; i<foundDevices.getCount(); i++)
+    JsonObject bt_network = data.createNestedObject("bluetooth");
+    for (int i = 0; i < foundDevices.getCount(); i++)
     {
       std::string mac = foundDevices.getDevice(i).getAddress().toString();
       bt_network[(String)mac.c_str()] = (int)foundDevices.getDevice(i).getRSSI();
     }
-    #else
+#else
     Serial.println("[ INFO ]\tBLE scan skipped (BLE disabled)..");
-    #endif // USE_BLE
+#endif // USE_BLE
 
-    #ifdef MODE_LEARNING
-      root["l"] = LOCATION;
-    #endif
-    
-    root.printTo(request);
+#if (MODE_LEARNING == 1)
+    root["l"] = LOCATION;
+#endif
 
-    #ifdef DEBUG
-    Serial.println(request);
-    #endif
-        
+    serializeJson(root, request);
+
+#if (DEBUG == 1)
+    Serial.println("[ DEBUG ]\t" + request);
+#endif
+
+#if (USE_HTTP_SSL == 1)
     WiFiClientSecure client;
-    const int httpsPort = 443;
-    if (!client.connect(host, httpsPort)) {
-      Serial.println("connection failed");
+#else
+    WiFiClient client;
+#endif
+    if (!client.connect(FIND_HOST, FIND_PORT)) {
+      Serial.println("[ WARN ]\tConnection to server failed...");
+      return;
     }
 
     // We now create a URI for the request
@@ -231,7 +219,7 @@ void SubmitWiFi(void)
 
     // This will send the request to the server
     client.print(String("POST ") + url + " HTTP/1.1\r\n" +
-                 "Host: " + host + "\r\n" +
+                 "Host: " + FIND_HOST + "\r\n" +
                  "Content-Type: application/json\r\n" +
                  "Content-Length: " + request.length() + "\r\n\r\n" +
                  request +
@@ -240,7 +228,7 @@ void SubmitWiFi(void)
 
     unsigned long timeout = millis();
     while (client.available() == 0) {
-      if (millis() - timeout > 2500) {
+      if (millis() - timeout > HTTP_TIMEOUT) {
         Serial.println("[ ERROR ]\tHTTP Client Timeout !");
         client.stop();
         return;
@@ -260,27 +248,63 @@ void SubmitWiFi(void)
       Serial.println(F("[ INFO ]\tGot a 200 OK."));
     }
 
-   char endOfHeaders[] = "\r\n\r\n";
-   if (!client.find(endOfHeaders)) {
-    Serial.println(F("[ ERROR ]\t Invalid Response"));
-    return;
-   }
-   else
-   {
-    Serial.println("[ INFO ]\tLooks like a valid response.");
-   }
+    char endOfHeaders[] = "\r\n\r\n";
+    if (!client.find(endOfHeaders)) {
+      Serial.println(F("[ ERROR ]\t Invalid Response"));
+      return;
+    }
+    else
+    {
+      Serial.println("[ INFO ]\tLooks like a valid response.");
+    }
 
-   Serial.println("[ INFO ]\tClosing connection.");
-   Serial.println("=============================================================");
+    Serial.println("[ INFO ]\tClosing connection.");
+    Serial.println("=============================================================");
 
-   #ifdef USE_DEEPSLEEP
-   esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
-   esp_deep_sleep_start();
-   #endif
+#if (USE_DEEPSLEEP == 1)
+#if defined(ARDUINO_ARCH_ESP8266)
+    ESP.deepSleep(TIME_TO_SLEEP * uS_TO_S_FACTOR);
+#elif defined(ARDUINO_ARCH_ESP32)
+    esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
+    esp_deep_sleep_start();
+#endif
+#endif
   }
 }
 
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+
+#if (USE_BLE == 1)
+  Serial.println("Find3 ESP client by DatanoiseTV (WiFi + BLE support.)");
+#else
+  Serial.println("Find3 ESP client by DatanoiseTV (WiFi support WITHOUT BLE.)");
+#endif
+
+  print_wakeup_reason();
+
+  Serial.print("[ INFO ]\tChipID is: ");
+  Serial.println("esp-" + GET_CHIP_ID());
+
+  wifiMulti.addAP(WIFI_SSID, WIFI_PSK);
+
+  Serial.println("[ INFO ]\tConnecting to WiFi..");
+  if (wifiMulti.run() == WL_CONNECTED) {
+    Serial.println("[ INFO ]\tWiFi connection established.");
+    Serial.print("[ INFO ]\tIP address: ");
+    Serial.println(WiFi.localIP());
+  }
+
+  timeClient.begin();
+}
+
 void loop() {
+  if (wifiMulti.run() != WL_CONNECTED) {
+    Serial.println("[ WARN ]\tWiFi not connected, retrying...");
+    delay(1000);
+    return;
+  }
+  timeClient.update();
   SubmitWiFi();
-  yield();
 }
